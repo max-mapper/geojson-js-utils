4cf5d6d4e5798064aee5c7ebf40e2e4b4fd48f75
Max Ogden <max@maxogden.com>
2010-12-13 13:35:11 -0800

    now automatically works in both browser and commonjs environments

--- geojson-utils.js
+++ geojson-utils.js
@@ -1,8 +1,13 @@
-exports.init = function() {
-  var g = {};
-
+(function() {
+  var gju = this.gju = {};
+  
+  // Export the geojson object for **CommonJS**
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = gju;
+  }
+  
   // adapted from http://www.kevlindev.com/gui/math/intersection/Intersection.js
-  g.lineStringsIntersect = function(l1, l2) {
+  gju.lineStringsIntersect = function(l1, l2) {
     var intersects = [];
     for (var i = 0; i <= l1.coordinates.length - 2; ++i) {
       for (var j = 0; j <= l2.coordinates.length - 2; ++j) {            
@@ -29,24 +34,8 @@ exports.init = function() {
     return intersects;
   }
 
-  g.scalarMultiply = function(xy, scalar) {
-    return [xy[0] * scalar, xy[1] * scalar];
-  }
-
-  g.subtractVector = function(xy, vector) {
-    return g.add(xy, g.scalarMultiply(vector, -1));
-  }
-
-  g.addVector = function(xy, vector) {
-    return [xy[0] + vector[0], xy[1] + vector[1]];
-  }
-
-  g.crossProduct = function(v1, v2) {
-    return v1[0] * v2[1] - v2[0] * v1[1];
-  }
-
   // adapted from http://jsfromhell.com/math/is-point-in-poly
-  g.pointInPolygon = function(point, polygon) {
+  gju.pointInPolygon = function(point, polygon) {
     var x = point.coordinates[1],
         y = point.coordinates[0],
         poly = polygon.coordinates[0]; //TODO: support polygons with holes
@@ -60,19 +49,19 @@ exports.init = function() {
     return c;
   }
   
-  g.numberToRadius = function(number) {
+  gju.numberToRadius = function(number) {
      return number * Math.PI / 180;
   }
 
-  g.numberToDegree = function(number) {
+  gju.numberToDegree = function(number) {
      return number * 180 / Math.PI;
   }
   
   // written with help from @tautologe 
-  g.drawCircle = function(radiusInMeters, centerPoint) {
+  gju.drawCircle = function(radiusInMeters, centerPoint) {
     var center = [centerPoint.coordinates[1], centerPoint.coordinates[0]],
         dist = (radiusInMeters / 1000) / 6371, // convert meters to radiant
-        radCenter = [g.numberToRadius(center[0]), g.numberToRadius(center[1])],
+        radCenter = [gju.numberToRadius(center[0]), gju.numberToRadius(center[1])],
         steps = 15, // 15 sided circle
         poly = [[center[0], center[1]]];
     for (var i = 0; i < steps + 1; i++) {
@@ -84,14 +73,14 @@ exports.init = function() {
                         Math.cos(dist) - Math.sin(radCenter[0]) *
                         Math.sin(lat));
         poly[i] = [];
-        poly[i][1] = g.numberToDegree(lat);
-        poly[i][0] = g.numberToDegree(lng);
+        poly[i][1] = gju.numberToDegree(lat);
+        poly[i][0] = gju.numberToDegree(lng);
     }
     return { "type": "Polygon",
              "coordinates": [poly] };
   }
 
-  g.rectangleCentroid = function(rectangle) {
+  gju.rectangleCentroid = function(rectangle) {
     var bbox = rectangle.coordinates[0];
     var xmin = bbox[0][0], ymin = bbox[0][1], xmax = bbox[1][0], ymax = bbox[1][1];
     var xwidth = xmax - xmin;
@@ -99,6 +88,5 @@ exports.init = function() {
     return { 'type': 'Point',
   	         'coordinates': [xmin + xwidth/2, ymin + ywidth/2] };
   }
-  
-  return g;
-}
+
+})();
