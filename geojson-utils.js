80b17ef6a2a02bcc3de2a5f54d071052e75a4a12
maxogden <max@maxogden.com>
2010-12-21 16:09:22 -0800

    adding haversine distance formula from http://www.movable-type.co.uk/scripts/latlong.html

--- geojson-utils.js
+++ geojson-utils.js
@@ -88,5 +88,18 @@
     return { 'type': 'Point',
   	         'coordinates': [xmin + xwidth/2, ymin + ywidth/2] };
   }
+  
+  // from http://www.movable-type.co.uk/scripts/latlong.html
+  gju.pointDistance = function(pt1, pt2) {
+    var lon1 = pt1.coordinates[0], lat1 = pt1.coordinates[1],
+        lon2 = pt2.coordinates[0], lat2 = pt2.coordinates[1],
+        dLat = gju.numberToRadius(lat2 - lat1),
+        dLon = gju.numberToRadius(lon2 - lon1),
+        a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(gju.numberToRadius(lat1)) * Math.cos(gju.numberToRadius(lat2)) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2),
+        c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    return (6371 * c) * 1000; // returns meters
+  }
 
 })();
