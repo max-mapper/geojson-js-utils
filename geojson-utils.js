8465301b1bd265913503bc78fe83f7dfbe75a89c
Tim Channell <tim.channell@gmail.com>
2015-10-14 09:46:04 -0400

    fixed point ordering for lineStringsIntersect, updated method name in readme

--- geojson-utils.js
+++ geojson-utils.js
@@ -12,20 +12,20 @@
     for (var i = 0; i <= l1.coordinates.length - 2; ++i) {
       for (var j = 0; j <= l2.coordinates.length - 2; ++j) {
         var a1 = {
-          x: l1.coordinates[i][1],
-          y: l1.coordinates[i][0]
+          x: l1.coordinates[i][0],
+          y: l1.coordinates[i][1]
         },
           a2 = {
-            x: l1.coordinates[i + 1][1],
-            y: l1.coordinates[i + 1][0]
+            x: l1.coordinates[i + 1][0],
+            y: l1.coordinates[i + 1][1]
           },
           b1 = {
-            x: l2.coordinates[j][1],
-            y: l2.coordinates[j][0]
+            x: l2.coordinates[j][0],
+            y: l2.coordinates[j][1]
           },
           b2 = {
-            x: l2.coordinates[j + 1][1],
-            y: l2.coordinates[j + 1][0]
+            x: l2.coordinates[j + 1][0],
+            y: l2.coordinates[j + 1][1]
           },
           ua_t = (b2.x - b1.x) * (a1.y - b1.y) - (b2.y - b1.y) * (a1.x - b1.x),
           ub_t = (a2.x - a1.x) * (a1.y - b1.y) - (a2.y - a1.y) * (a1.x - b1.x),
